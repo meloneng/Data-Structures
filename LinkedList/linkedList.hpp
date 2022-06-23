b3908edcee8299a9922d1c92ac5a329232a4b4fb
meloneng <melon_eng@protonmail.com>
2022-06-23 14:59:41 -0300

    insertion on linkedlist

--- LinkedList/linkedList.hpp
+++ LinkedList/linkedList.hpp
@@ -13,10 +13,21 @@ typedef struct List{
 
 Llist* newList(){
     Llist* newlist = (Llist*) malloc(sizeof(Llist));
+    newlist->head = nullptr;
+    newlist->listLength = 0;
     return newlist;
 }
 
 Node* newNode(int data){
     Node* newnode = (Node*) malloc(sizeof(Node));
+    newnode->next = nullptr;
+    newnode->value = NULL;
     return newnode;
+}
+
+
+void newNode(Llist** list, int value){
+    if((list->head) == nullptr){
+
+    }
 }
